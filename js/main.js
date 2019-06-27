3c660f173ad158d7cdf5835a8089a834e4e744ed
Jerry Diaz <luis.diaz@cceo.com.mx>
2019-06-27 15:26:44 -0500

    Static table changed to dinamyc

--- js/main.js
+++ js/main.js
@@ -2,12 +2,27 @@ var app = new Vue({
     el : '#app',
     data : {
         list : [
-            {name: "PP Chuy", average: '10', status: true},
-            {name: "Airel Jaramillo", average: '6', status: false},
-            {name: "Leisa Rubi", average: '6.5', status: true}
+        
         ],
         name: '',
         average: ''
+    },
+//Verification methods for status in grades 
+    methods : {
+        addGrade: function() {
+            statusA = false;
+            if(this.average >= 6.5){
+                statusA = true;
+            }
+            if(this.name!="" && this.average!=""){
+                this.list.push({name:this.name, average:this.average, status:statusA});
+                this.name = "";
+                this.average = "";
+            }
+            else{
+                alert("Verifique que se han llenado los campos");
+            }
+        } 
     }
     
 })
