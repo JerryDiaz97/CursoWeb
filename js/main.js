3ce4e17ac0032f344cc6cd58fd942c79614e6ba1
Jerry Diaz <luis.diaz@cceo.com.mx>
2019-06-27 14:43:34 -0500

    Static tables completed and diferent color for status

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var app = new Vue({
     el : '#app',
     data : {
-        message: 'Welcome to Vue.js'
+        list : [
+            {name: "PP Chuy", average: '10', status: true},
+            {name: "Airel Jaramillo", average: '6', status: false},
+            {name: "Leisa Rubi", average: '6.5', status: true}
+        ],
     }
     
 })
